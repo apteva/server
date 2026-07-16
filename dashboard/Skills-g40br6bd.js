28d576efe50af8857764565e2221c81044022ff8
Apteva <contact@apteva.ai>
2026-07-16 09:01:43 +0200

    server: refresh dashboard telemetry paint embed

---
+++ dashboard/Skills-g40br6bd.js
@@ -0,0 +1,4 @@
+import{a,b}from"./main-mptjjfpn.js";import"./main-tbvfvfjc.js";import"./main-45wrcmr0.js";export{b as StatusPill,a as Skills};
+
+//# debugId=144BE380E66EA18264756E2164756E21
+//# sourceMappingURL=Skills-g40br6bd.js.map
